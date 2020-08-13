e3a2194f2d2b55ede17034bfca20ee564d6a2965
Brilly <brillyanto.m@gmail.com>
2020-08-13 12:00:48 +0530

    asdfa lskdfja sld;fsdf On branch master

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
+// this is my second fix
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
